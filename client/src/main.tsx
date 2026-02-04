2f7424c9faa5f1d03c9284db7684e85f5eea91a9
vimla01 <224776616+vimla01@users.noreply.github.com>
2026-02-05 04:19:15 +0530

    update main & app file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SiteLayout from "./layouts/SiteLayout";
 import App from "./App";
+import GetStarted from "./pages/GetStarted";
+import Contact from "./pages/Contact";
+import Community from "./pages/Community";
+
 import "./styles.css";
+import About from "./pages/about";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<SiteLayout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/get-started" element={<GetStarted />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/about" element={<About />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
